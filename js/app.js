41cd32b0feaafeccd8a2e3afb1ef950f4f46bd68
siphiwe <siphiwemanda@gmail.com>
2020-07-29 21:16:04 +0100

    adding js model, refactor code

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const model={
+    Animals:[
+        {
+            animalName: 'The Eagle',
+            img: '../img/eagle.png',
+            level: 4,
+            description: "\t\t\t\tEagles are some of the largest birds. They are at the top of the food chain. Eagles have amazing eyesight and can detect prey up to two miles away.\n",
+            Training:20,
+            Speed: 16,
+            Cost:150,
+        },
+        {
+            name: 'Aspen',
+            img: 'img/aspen.jpg',
+        },
+        {
+            name: 'max',
+            img: 'img/max.jpg',
+        },
+        {
+            name: 'Jack',
+            img: 'img/jack.jpg',
+        },
+        {
+            name: 'bindi',
+            img: 'img/bindi.jpg',
+        }
+    ],
+};
