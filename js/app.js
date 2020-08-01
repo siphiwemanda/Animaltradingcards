f78a8d742b43fdf834645bfeca2800f1a1254dc4
siphiwe <siphiwemanda@gmail.com>
2020-08-01 14:59:38 +0100

    New object

--- js/app.js
+++ js/app.js
@@ -1,40 +1,21 @@
-const model={
-    Animals:[
-        {
-            animalName: 'The Eagle',
-            img: '../img/eagle.png',
-            level: 4,
-            description: "\t\t\t\tEagles are some of the largest birds. They are at the top of the food chain. Eagles have amazing eyesight and can detect prey up to two miles away.\n",
-            Training:20,
-            Speed: 16,
-            Cost:150,
-        },
-        {
-            animalName: 'The Panda',
-            img: '../img/panda.png',
-            level: 4,
-            description: "\t\t\t\tEagles are some of the largest birds. They are at the top of the food chain. Eagles have amazing eyesight and can detect prey up to two miles away.\n",
-            Training:20,
-            Speed: 16,
-            Cost:150,
-        },
-        {
-            animalName: 'The Bear',
-            img: '../img/bear.png',
-            level: 4,
-            description: "\t\t\t\tEagles are some of the largest birds. They are at the top of the food chain. Eagles have amazing eyesight and can detect prey up to two miles away.\n",
-            Training:20,
-            Speed: 16,
-            Cost:150,
-        },
-        {
-            animalName: 'The Doggo',
-            img: '../img/dog.png',
-            level: 4,
-            description: "\t\t\t\tEagles are some of the largest birds. They are at the top of the food chain. Eagles have amazing eyesight and can detect prey up to two miles away.\n",
-            Training:20,
-            Speed: 16,
-            Cost:150,
-        }
-    ],
-};
+const Animal = (Name, Img, Level, Description, Training, Speed, Cost) => {
+    return{
+        Name,
+        Img,
+        Level,
+        Description,
+        Training,
+        Speed,
+        Cost,
+    }
+}
+const EagleDescription = "Eagles are some of the largest birds. They are at the top of the food chain. Eagles have amazing eyesight and can detect prey up to two miles away"
+const Panda = Animal('The Panda', '../img/panda.js', 300, 'hello i am a panda', 800,22,800)
+const Eagle = Animal('The Eagle','../img/eagle.png', 4, EagleDescription, 20,16,150 )
+const Bear = Animal('The Bear', '../img/bear.png', 4,'ima bear',20,16,150)
+const Dog = Animal('The Doggo', '..img/dog.png',4,'im a dog',4,5,8000)
+
+console.log(Panda)
+console.log(Eagle)
+console.log(Bear)
+console.log(Dog)
